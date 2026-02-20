fd5b1ef4ea5bcc17f202913527a0bcfae436f381
AnupamSaxenaa <saxenaanupam2004@gmail.com>
2026-02-20 20:02:14 +0530

    Use Render backend URL as default API base

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,6 @@ const { authenticate, requireRole } = require('../middleware/auth');
 const { listUsers, getUserById, updateUser, deactivateUser, getAllDepartments } = require('../controllers/usersController');
 
 const router = express.Router();
-
 router.get('/', authenticate, requireRole('admin'), listUsers);
 router.get('/departments/all', getAllDepartments);
 router.get('/:id', authenticate, getUserById);
