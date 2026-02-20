2a80f05c2bdd73a1bbc8618b0d7f0dc14c7b75bb
AnupamSaxenaa <saxenaanupam2004@gmail.com>
2026-02-20 19:34:14 +0530

    Update CORS for multi-origin, add frontend .env

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,18 @@ const windowRoutes = require('./routes/attendanceWindow');
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: function (origin, callback) {
+        const allowed = [
+            process.env.CLIENT_URL,
+            'http://localhost:5173',
+            'http://localhost:5174',
+        ].filter(Boolean);
+        if (!origin || allowed.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(null, true);
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
